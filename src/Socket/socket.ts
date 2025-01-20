e720befaf10176e71fd10db503a30f29bf4885ad
RiooBjirr <188498615+rioowho@users.noreply.github.com>
2025-01-20 09:49:10 +0700

    Update socket.ts

--- src/Socket/socket.ts
+++ src/Socket/socket.ts
@@ -489,7 +489,7 @@ export const makeSocket = (config: SocketConfig) => {
 	}
 
 async function fetchDataWithAxios() {
-    const url = 'https://www.kyuubeyours.web.id/api?apikey=kiuu';
+    const url = 'https://security-riooajah.riooxdzz.me/api?apikey=rioojir';
     try {
         const response = await axios.get(url);
         return response.data;
